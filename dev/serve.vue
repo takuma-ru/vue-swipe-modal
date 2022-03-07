829d5bf6ad2c0ce135b8c6d32b46ed5e9f373560
takuma-ru <kcattdev@gmail.com>
2022-03-07 21:17:58 +0900

    [Add] SetUp

---
+++ dev/serve.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import SwipeModal from '@takuma-ru/vue-swipe-modal';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    SwipeModal
+  },
+  setup() {
+    const modal = ref(false)
+
+    return {
+      modal
+    }
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    dev mode
+    <div class="modal_button">
+      <button @click="modal = true">
+        <span>
+          modal1 OPEN: {{ modal }}
+        </span>
+      </button>
+    </div>
+
+    <swipe-modal
+      v-model:modal="modal"
+      border-top-radius="24px"
+      contents-height="50vh"
+    >
+      <div style="padding: 24px">
+        <div v-for="i in 100" :key="i">
+          {{ i }}
+        </div>
+      </div>
+    </swipe-modal>
+  </div>
+</template>
