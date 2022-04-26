1e4d8c656776909365b9b693198e578559d248b4
takuma-ru <kcattdev@gmail.com>
2022-04-26 10:58:34 +0900

    [Add] rollup plugin

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [{
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      }]
+    }
+  }
+}
