6cf962d4481df72b5c59db81fbf800044941cb19
takuma-ru <kcattdev@gmail.com>
2022-07-12 17:05:25 +0900

    [Add] Set Up

--- vue2-app/vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../docs',
-  assetsDir: './',
-  publicPath: './',
-})
