82e9684ae17ca4760084908b8696a21a8b46bada
takumaru <49429291+takuma-ru@users.noreply.github.com>
2022-11-29 14:36:50 +0900

    Update nuxt.config.ts

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,6 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
+
   app: {
     head: {
       title: '@takuma-ru/vue-swipe-modal',
