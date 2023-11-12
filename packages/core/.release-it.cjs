91b1b9b86eb0a8165d83899f550b9634eccb112c
takuma-ru <kcattdev@gmail.com>
2023-11-12 15:39:45 +0900

    💚 publish ci

---
+++ packages/core/.release-it.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  npm: {
+    publish: true,
+  },
+  github: {
+    release: true,
+  },
+  git: {
+    requireCleanWorkingDir: false,
+    addFiles: ["package.json", "CHANGELOG.md"],
+    commitMessage: ":bookmark: release @takuma-ru/vue-swipe-modal@${version}",
+  },
+  plugins: {},
+};
