fe1880070f9186de0054f9bfd70706031faa9027
takuma-ru <kanekotakuma@team-lab.com>
2024-05-17 16:22:22 +0900

    🎉 vue project

---
+++ packages/vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import WebBottomSheet from "./components/WebBottomSheet.vue";
+import { ref } from "vue";
+
+const isOpen = ref(false);
+</script>
+
+<template>
+  <button @click="isOpen = true">open</button>
+
+  <WebBottomSheet :open="isOpen" snap-point="50%">
+    <button @click="isOpen = false">close</button>
+  </WebBottomSheet>
+</template>
+
+<style scoped>
+</style>
---
+++ packages/vue/src/components/WebBottomSheet.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import "core"
+
+const props = defineProps<{
+  open: boolean;
+  snapPoint?: "auto" | String;
+}>();
+</script>
+
+<template>
+  <web-bottom-sheet :open="props.open" :snap-point="props.snapPoint">
+    <slot></slot>
+  </web-bottom-sheet>
+</template>
