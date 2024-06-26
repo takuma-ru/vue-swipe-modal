54d0386e1887d652f4a5459b24aa63868b2a3567
takuma-ru <kanekotakuma@team-lab.com>
2024-06-26 12:59:07 +0900

    ➕ vue-swipe-modal in @web-bottom-sheet/vue

--- packages/vue/src/main.ts
+++ packages/vue/src/main.ts
@@ -1,5 +1,6 @@
 import "@web-bottom-sheet/core";
+import SwipeModal from "@takuma-ru/vue-swipe-modal";
 import BottomSheet from "./components/BottomSheet.vue";
 
-export { BottomSheet };
+export { BottomSheet, SwipeModal };
 export default BottomSheet;
