65b1df52dfd116e738c7d6752641dc75f34130aa
takuma-ru <kanekotakuma@team-lab.com>
2024-07-18 13:45:52 +0900

    ✨ package info for npm lib

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
 
   build: {
     outDir: "./dist",
